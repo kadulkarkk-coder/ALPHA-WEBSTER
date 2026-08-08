a2b3565fa41f286cff5e5574825c859dea2e5117
kadulkarkk-coder <kadulkarkk@gmail.com>
2026-08-08 18:51:03 +0530

    Sprint 36.1 add voice subsystem package

---
+++ core/voice/__init__.py
@@ -0,0 +1,13 @@
+"""Webster Alpha voice subsystem."""
+
+from core.voice.engine import VoiceEngine
+from core.voice.manager import VoiceManager
+from core.voice.listener import VoiceListener
+from core.voice.speaker import VoiceSpeaker
+
+__all__ = [
+    "VoiceEngine",
+    "VoiceManager",
+    "VoiceListener",
+    "VoiceSpeaker",
+]
