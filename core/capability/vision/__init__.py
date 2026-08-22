f8290ac1f33621de26f622e53939b8f9f9acad18
kadulkarkk-coder <kadulkarkk@gmail.com>
2026-08-22 22:57:18 +0530

    feat: initialize vision capability package

---
+++ core/capability/vision/__init__.py
@@ -0,0 +1,15 @@
+"""Vision capabilities exposed by Webster."""
+
+from .capabilities import (
+    VisionDisableCapability,
+    VisionEnableCapability,
+    VisionScreenCapability,
+    VisionStatusCapability,
+)
+
+__all__ = [
+    "VisionDisableCapability",
+    "VisionEnableCapability",
+    "VisionScreenCapability",
+    "VisionStatusCapability",
+]
